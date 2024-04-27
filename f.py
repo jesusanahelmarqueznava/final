54cb61e36baee8ceed060b409fb4d4c34a5c0d81
jesusanahelmarqueznava <111540584+jesusanahelmarqueznava@users.noreply.github.com>
2024-04-26 19:16:43 -0600

    f.py

--- f.py
+++ f.py
@@ -1,27 +1,54 @@
 import streamlit as st
+import nbformat
+from nbconvert.preprocessors import ExecutePreprocessor
+from nbconvert import HTMLExporter
 
-def show_code(file_path):
-    with open(file_path, "r") as file:
-        code = file.read()
-    st.code(code, language="python")
+def execute_notebook(notebook):
+    execute_preprocessor = ExecutePreprocessor(timeout=-1, kernel_name='python3')
+    execute_preprocessor.preprocess(notebook, {'metadata': {'path': './'}})
+
+def option_one():
+    st.write("Has seleccionado la opción 1")
+    notebook_path = "Patrones.ipynb"
+    notebook = nbformat.read(notebook_path, as_version=4)
+    execute_notebook(notebook)
+    html_exporter = HTMLExporter()
+    (body, _) = html_exporter.from_notebook_node(notebook)
+    st.write(body, unsafe_allow_html=True)
+
+def option_two():
+    st.write("Has seleccionado la opción 2")
+    notebook_path = "Anomalias.ipynb"
+    notebook = nbformat.read(notebook_path, as_version=4)
+    execute_notebook(notebook)
+    html_exporter = HTMLExporter()
+    (body, _) = html_exporter.from_notebook_node(notebook)
+    st.write(body, unsafe_allow_html=True)
+
+def option_three():
+    st.write("Has seleccionado la opción 3")
+    notebook_path = "Analisis_Estaionariedad.ipynb"
+    notebook = nbformat.read(notebook_path, as_version=4)
+    execute_notebook(notebook)
+    html_exporter = HTMLExporter()
+    (body, _) = html_exporter.from_notebook_node(notebook)
+    st.write(body, unsafe_allow_html=True)
 
 def main():
-    st.title("Menú con Streamlit")
+    st.title("Menú con Ejecución de Notebook en Streamlit")
 
     options = ["Opción 1", "Opción 2", "Opción 3"]
     choice = st.sidebar.selectbox("Selecciona una opción", options)
 
     if choice == "Opción 1":
-        st.subheader("Has seleccionado la opción 1")
-        show_code("Patrones.ipynb")
+        option_one()
     elif choice == "Opción 2":
-        st.write("Has seleccionado la opción 2")
-        show_code("Anomalias.ipynb")
+        option_two()
     elif choice == "Opción 3":
-        st.write("Has seleccionado la opción 3")
-        show_code("Analisis_Estacionariedad.ipynb")
+        option_three()
 
 if __name__ == "__main__":
     main()
+
 
 
